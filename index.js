dd013c7c1272aa21cefd84913644a916c640e648
Alvaro Gallo <galloalvaro5@gmail.com>
2023-05-02 23:09:06 -0600

    WIP navbar on mobile screen

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ window.onscroll = function () {
     navbar.classList.remove("nav-shadow");
   }
 };
+function openOverlay() {
+    let overlay = document.querySelector(".overlay");
+    overlay.classList.add("overlay-active");
+    let body = document.querySelector("body");
+    body.classList.add("no-scroll");
+}
+function closeOverlay() {
+    let overlay = document.querySelector(".overlay");
+    overlay.classList.remove("overlay-active");
+    let body = document.querySelector("body");
+    body.classList.remove("no-scroll");
+}
